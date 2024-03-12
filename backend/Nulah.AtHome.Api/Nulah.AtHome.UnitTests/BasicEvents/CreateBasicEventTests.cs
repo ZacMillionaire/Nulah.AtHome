b31357089f731db8473e1d64a23d94d0527a8016
Scott <scott.schultz@jotunga.com>
2024-03-12 15:44:04 +1000

    Update tests - validate tags are created - validate duplicate events with pre-existing tags do not recreate tags

--- backend/Nulah.AtHome.Api/Nulah.AtHome.UnitTests/BasicEvents/CreateBasicEventTests.cs
+++ backend/Nulah.AtHome.Api/Nulah.AtHome.UnitTests/BasicEvents/CreateBasicEventTests.cs
@@ -76,26 +76,76 @@ public class CreateBasicEventTests : TestBase<CreateBasicEventFixture>
 		Assert.Equal("Start date cannot be exactly on or after end date", exception.Message);
 	}
 
+	[Fact]
+	public async void CreateEventWithDuplicateTags()
+	{
+		var tagList = new List<string>()
+		{
+			$"test-{Guid.NewGuid()}",
+			$"test-{Guid.NewGuid()}",
+			$"test-{Guid.NewGuid()}"
+		};
+
+		var firstEventRequest = new NewBasicEventRequest()
+		{
+			Description = $"First Test Description {Guid.NewGuid()}",
+			Start = DateTimeOffset.Now,
+			Tags = tagList
+		};
+
+		var firstCreatedBasicEvent = await TestFixture.CreateBasicEvent(firstEventRequest);
+
+		AssertEventDetails(firstEventRequest, firstCreatedBasicEvent);
+
+		Assert.All(firstCreatedBasicEvent.Tags, x =>
+		{
+			Assert.Contains(x.Name, tagList);
+			Assert.NotEqual(0, x.Id);
+			Assert.NotEqual(0u, x.Version);
+		});
+
+		var secondEventRequest = new NewBasicEventRequest()
+		{
+			Description = $"Second Test Description {Guid.NewGuid()}",
+			Start = DateTimeOffset.Now.AddDays(1),
+			Tags = tagList
+		};
+
+		var secondCreatedBasicEvent = await TestFixture.CreateBasicEvent(secondEventRequest);
+
+		AssertEventDetails(secondEventRequest, secondCreatedBasicEvent);
+
+		Assert.All(secondCreatedBasicEvent.Tags, x =>
+		{
+			Assert.Contains(x.Name, tagList);
+			Assert.NotEqual(0, x.Id);
+			Assert.NotEqual(0u, x.Version);
+		});
+
+		Assert.All(secondCreatedBasicEvent.Tags, x =>
+		{
+			Assert.NotNull(firstCreatedBasicEvent.Tags.FirstOrDefault(y =>
+				y.Name == x.Name
+				&& y.Id == x.Id
+				&& y.Version == x.Version
+			));
+		});
+	}
+
 	[Theory]
 	[MemberData(nameof(CreateBasicEventFixture.EventsToCreate), MemberType = typeof(CreateBasicEventFixture))]
 	public async void CreateValidEvents((string description, DateTimeOffset startDate, DateTimeOffset? endDate, List<string>? tags) testData)
 	{
-		var createdBasicEvent = await TestFixture.CreateBasicEvent(new NewBasicEventRequest()
+		var createEventRequest = new NewBasicEventRequest()
 		{
 			Description = testData.description,
 			Start = testData.startDate,
 			End = testData.endDate,
 			Tags = testData.tags
-		});
+		};
+		var createdBasicEvent = await TestFixture.CreateBasicEvent(createEventRequest);
 
-		Assert.NotEqual(0, createdBasicEvent.Id);
-		Assert.NotEqual(0u, createdBasicEvent.Version);
-		Assert.NotEqual(DateTimeOffset.Now, createdBasicEvent.CreatedUtc);
-		Assert.NotEqual(DateTimeOffset.Now, createdBasicEvent.UpdatedUtc);
-		Assert.Equal(createdBasicEvent.CreatedUtc, createdBasicEvent.UpdatedUtc);
-		Assert.Equal(testData.description, createdBasicEvent.Description);
-		Assert.Equal(testData.startDate, createdBasicEvent.Start);
-		Assert.Equal(testData.endDate, createdBasicEvent.End);
+		AssertEventDetails(createEventRequest, createdBasicEvent);
 
 		if (testData.tags != null)
 		{
@@ -105,10 +155,20 @@ public class CreateBasicEventTests : TestBase<CreateBasicEventFixture>
 				Assert.Contains(x.Name, testData.tags);
 				Assert.NotEqual(0, x.Id);
 				Assert.NotEqual(0u, x.Version);
-				Assert.NotEqual(DateTimeOffset.Now, x.CreatedUtc);
-				Assert.NotEqual(DateTimeOffset.Now, x.UpdatedUtc);
 			});
 		}
+	}
+
+	private void AssertEventDetails(NewBasicEventRequest eventCreationRequest, BasicEventDto createdEvent)
+	{
+		Assert.NotEqual(0, createdEvent.Id);
+		Assert.NotEqual(0u, createdEvent.Version);
+		Assert.NotEqual(DateTimeOffset.Now, createdEvent.CreatedUtc);
+		Assert.NotEqual(DateTimeOffset.Now, createdEvent.UpdatedUtc);
+		Assert.Equal(createdEvent.CreatedUtc, createdEvent.UpdatedUtc);
+		Assert.Equal(eventCreationRequest.Description, createdEvent.Description);
+		Assert.Equal(eventCreationRequest.Start, createdEvent.Start);
+		Assert.Equal(eventCreationRequest.End, createdEvent.End);
 	}
 }
 
@@ -118,9 +178,18 @@ public class CreateBasicEventFixture : DatabaseBackedTestFixture
 
 	public static TheoryData<(string description, DateTimeOffset startDate, DateTimeOffset? endDate, List<string>? tags)> EventsToCreate() => new()
 	{
-		("Test Description", DateTimeOffset.Now, null, null),
-		("A description", DateTimeOffset.Now, DateTimeOffset.Now.AddDays(1), null),
-		("A description for longer text", DateTimeOffset.Now.AddMinutes(1), DateTimeOffset.Now.AddDays(1), null),
+		(
+			"Test Description", DateTimeOffset.Now, null,
+			new List<string>() { $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}" }
+		),
+		(
+			"A description", DateTimeOffset.Now, DateTimeOffset.Now.AddDays(1),
+			new List<string>() { $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}" }
+		),
+		(
+			"A description for longer text", DateTimeOffset.Now.AddMinutes(1), DateTimeOffset.Now.AddDays(1),
+			new List<string>() { $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}", $"test-{Guid.NewGuid()}" }
+		),
 	};
 
 	public async Task<BasicEventDto> CreateBasicEvent(NewBasicEventRequest basicEventRequest)
